811cad5bead5224b42d93084510fc297f6834b24
raghu prasad <prasadraghuks@gmail.com>
2023-01-06 10:51:46 +0530

    js

---
+++ programs/web/operators.js
@@ -0,0 +1,14 @@
+console.log('operators')
+
+n1=100
+n2=200
+res = n1+n2
+console.log('result of addition is ',res)
+res = n1>n2
+console.log(n1, ">",n2," " ,res)
+n3=200
+n4='200'
+console.log( 'n3 == n4 ',n3==n4)
+console.log( 'n3 === n4 ',n3===n4)
+res = n1<n2 && n3>=n2
+console.log( 'n1<n2 && n3>=n2 ',res)
