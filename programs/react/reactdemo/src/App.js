71f06339caa32dec97b8f8cbb704696e1b522c7e
raghu prasad <prasadraghuks@gmail.com>
2023-01-07 14:39:14 +0530

    react

--- programs/react/reactdemo/src/App.js
+++ programs/react/reactdemo/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
-    <div className="App">      
+    <div className="App">  
       
       <Header/>
+      <Content/>
     </div>
   );
 }
---
+++ programs/react/reactdemo/src/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Content=()=>{
+
+    const course = "react"
+    const wishlist=["Python","Flask","Pandas"]
+
+    const emp = {code:1,name:'ravi',dept:'IT'}
+    
+    return(
+        <div>
+            <h1>Course is {course}</h1>
+            <h1>My wishlist is {wishlist}</h1>
+            <h1>Employee Code : {emp.code} Name : {emp.name} Dept : {emp.dept}</h1>
+
+        </div>
+
+    )
+}
+
+export default Content
