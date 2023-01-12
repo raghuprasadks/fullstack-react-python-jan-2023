74ca9fbafc9f1d7cf3b290510e823b2af0b8db81
raghu prasad <prasadraghuks@gmail.com>
2023-01-12 15:33:39 +0530

    updated

--- programs/react/reactdemo/src/components/Todos.js
+++ programs/react/reactdemo/src/components/Todos.js
@@ -14,7 +14,31 @@ const Todos=()=>{
     },[])
     return(
         <div>
+        <div>
             <h1>Total Todos : {todos.length}</h1>
+        </div>
+        <div>
+        <table className='todocss'>
+            <tr>
+            <th>User Id</th>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Completed</th>
+                        
+            </tr>
+            {todos.map((todo)=>
+         <tr key={todo.id}>
+             <td>{todo.userId}</td>
+             <td>{todo.id}</td>
+             <td>{todo.title}</td>
+             <td>{todo.completed.toString()}</td>                    
+             
+         </tr>
+         )}
+        
+        </table>
+        </div>
+
         </div>
     )
 }
